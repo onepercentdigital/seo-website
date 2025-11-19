5749ed6dd5bbd4da342aa59212e7205aba0114d5
Austin Falk <38740468+austinfalk@users.noreply.github.com>
2025-11-19 09:10:47 -0700

    fix(__root.tsx): removed notfoundcomp from router config and kept in root route

--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,6 @@
 import * as Sentry from '@sentry/tanstackstart-react';
 import { createRouter } from '@tanstack/react-router';
 import { setupRouterSsrQueryIntegration } from '@tanstack/react-router-ssr-query';
-import { NotFound } from './components/NotFound';
 import * as TanstackQuery from './integrations/tanstack-query/root-provider';
 
 // Import the generated route tree
@@ -15,7 +14,6 @@ export const getRouter = () => {
     routeTree,
     context: { ...rqContext },
     defaultPreload: 'intent',
-    notFoundComponent: NotFound,
     Wrap: (props: { children: React.ReactNode }) => {
       return (
         <TanstackQuery.Provider {...rqContext}>
